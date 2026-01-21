cbe1d43c7c7665d4a839ffee12d66c8f93f9c91f
windy <128990874+FishyRamen@users.noreply.github.com>
2026-01-21 17:34:55 -0500

    Change redirect option to false

--- api/main.py
+++ api/main.py
@@ -50,7 +50,7 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": true, # Redirect to a webpage?
+        "redirect": false, # Redirect to a webpage?
         "page": "https://www.youtube.com/watch?v=XEFZ30Cvdnc" # Link to the webpage to redirect to 
     },
 
