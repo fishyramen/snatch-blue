e4cd490d3bc0153491b114df28ab284ffee8a88d
fishyramen <128990874+fishyramen@users.noreply.github.com>
2026-01-22 22:27:57 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ config = {
 
     # CUSTOMIZATION #
     "username": "snatch blue", # Set this to the name you want the webhook to have
-    "color": 0x29FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
